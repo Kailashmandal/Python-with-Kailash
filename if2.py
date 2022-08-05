97f29e6740af5cc1ca3679fbfc013446f6157ed6
Kailashmandal <hashtagkailash@gmail.com>
2022-08-05 14:05:15 +0530

    Day 6 pyhton

---
+++ if2.py
@@ -0,0 +1,29 @@
+# in keyword : is used to find whether a char is present in string or not 
+
+name = "Kailash"
+
+if 'a' in name:
+    print("a is present in name ")
+else:
+    print("a is not prsent in name ")
+
+############
+
+is_present = 'x' in name # this will give you true false value 
+
+print(is_present) 
+
+
+# checking whether a string is empty or not 
+name = "abc"
+
+if name : # this true if string contain any char
+    print(f"your name is {name}")
+
+
+str=""
+
+if str:
+    print(f"str is {str}")
+else:
+    print("str is empty")
---
+++ loop1.py
@@ -0,0 +1,8 @@
+# for loop is used with range function
+
+# syntax :        i in range (start value of i , end value of i +1 ) 
+
+for i in range(1,11): # i starts from 0 and end at 9 it doesn't go to 10 range increases the value of i upto 9 only and then terminates the loop
+    print(f"Hey Kailash !! {i} ")
+
+print("now i is :",i)
