39ec5a938b74ef2bd2327642db5c53d35dd37886
Kailashmandal <hashtagkailash@gmail.com>
2022-08-17 23:30:54 +0530

    Day 9 python

---
+++ list5.py
@@ -0,0 +1,4 @@
+### taking list as input from user
+
+list =[int(x) for x in input().split()]
+print(list)
---
+++ practice9.py
@@ -0,0 +1,62 @@
+#  Write a Python program to sum all the items in a list. 
+
+# list =[]
+# i=0
+# while i<5:
+#     list.append( int(input()) )
+#     i+=1
+
+# sum=0
+# for i in list:
+#     sum+=i
+
+# print(sum)
+
+
+
+
+# # Write a Python program to multiply all the items in a list
+# list = [ int(x) for x in input().split()] # takign list input in single line
+
+# mul=1
+# for x in list:
+#    mul*=x
+
+# print(mul)
+
+
+
+
+# Write a Python program to get the largest number from a list.
+
+
+# list = [ int(x) for x in input().split()]
+
+# max = list[0]
+# min=max
+# for x in list:
+#     if x>max:
+#         max =x
+#     elif x<min:
+#         min=x
+        
+# print("min is :",min)
+# print("max is :",max)
+
+
+
+# Write a Python program to count the number of 
+# strings where the string length is 2 or more and 
+# the first and last character are same from a given list of strings.
+
+
+# list = [str for str in input().split()]
+# count=0
+# for str in list :
+#     if len(str)>=2 and ( str[0]==str[-1]):
+#         count+=1
+
+# print(count)
+
+
+
