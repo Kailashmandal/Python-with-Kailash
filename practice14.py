b4d30034bf8ecccb5d2bfc0a45ced160ee2c11db
Kailashmandal <hashtagkailash@gmail.com>
2022-08-27 23:50:16 +0530

    Day 14 python

---
+++ practice14.py
@@ -0,0 +1,38 @@
+#  Write a Python program to change the 
+# position of every n-th value with the (n+1)th in a list.
+list = [1,0,4,5,6,7,8]
+
+# for i in range( 0,len(list)-1,2):
+#     temp=list[i]
+#     list[i]=list[i+1]
+#     list[i+1]=temp
+    
+
+# print( list )
+
+# Write a Python program to convert a list of multiple integers into a single integer
+
+# list= [11, 33, 50]
+# st=''
+# for x in list:
+#     st=st+str(x)
+
+
+# print( int(st))
+
+# Write a Python program to extract common index elements from more than one given list.
+# list1=[0,0,4,6,7,8,8]
+# list2=[5,0,4,6,7,90,8]
+
+# r= len(list1) if len(list1)<len(list2) else len(list2)
+# common_value_list=[]
+# # print(r)
+# for i in range(r):
+#     if list1[i]==list2[i]:
+#         # print(i)
+#         common_value_list.append(list1[i])
+
+# print(common_value_list)
+
+
+
